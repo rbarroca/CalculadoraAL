130b7c1093026a201726ea1c6d82aaa19dbe1de9
Rui Barroca <barroca23@gmail.com>
2026-01-18 22:12:29 +0000

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'icons': ['lucide-react']
+        }
+      }
+    }
+  }
 });
